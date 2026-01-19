125780ed6240ee4ee2a2e26a33c13b603e3c264d
2011Alexandrealvarenga <2011brunofernandes@gmail.com>
2026-01-19 19:14:26 -0300

    Title: Specialities

--- src/components/parts-site/Specialities.tsx
+++ src/components/parts-site/Specialities.tsx
@@ -4,88 +4,85 @@ import Img_icon_checked from '../../assets/img/check-icon.svg';
 export default function Specialities() {
   return (
     <div>
-      <hr className="split-section"></hr>
-        <section id="specialist">
-          <div className="container">
-            <div className="row">
-              <div className="col-12 text-center">
-                <div className="content-title-section">
-                  <h3 className="title-section">NOSSAS ESPECIALIDADES</h3>
-                  <div className="hr-line-title"></div>
-                </div>
+      <section id="specialist">
+        <div className="container">
+          <div className="row">
+            <div className="col-12 text-center">
+              <div className="content-title-section">
+                <h3 className="title-section">NOSSAS ESPECIALIDADES</h3>
+                <div className="hr-line-title"></div>
               </div>
             </div>
-            <div className="row">
-              <div className="col-md-12">
-                <div className="three-itens">
-                  {/* item */}
-                  <div className="itens-card">
-                    <div className="content-title">
-                      <img src={Img_icon.src} className="img-item-icon" />
-                      <h2 className="title">Terraplanagem</h2>
+          </div>
+          <div className="row">
+            <div className="col-md-12">
+              <div className="three-itens">
+                <div className="itens-card">
+                  <div className="content-title">
+                    <img src={Img_icon.src} className="img-item-icon"/>
+                    <h2 className="title">Terraplanagem</h2>
+                  </div>
+                  <div className="content-itens">
+                    <div className="itens">
+                      <img src={Img_icon_checked.src} className="img-item-icon"/>
+                      <span className="specialist-position">Escavações em solo e rocha.</span>
                     </div>
-                    <div className="content-itens">
-                      <div className="itens">
-                        <img src={Img_icon_checked.src} className="icon-check" />
-                        <span className="specialist-position">Escavações em solo e rocha.</span>
-                      </div>
-                      <div className="itens">
-                        <img src={Img_icon_checked.src} className="icon-check" />
-                        <span className="specialist-position">Execução de camadas de reforço de camadas de reforço e subleito tratado.</span>
-                      </div>
-                      <div className="itens">
-                        <img src={Img_icon_checked.src} className="icon-check" />
-                        <span className="specialist-position">Escavações em solo e rocha.</span>
-                      </div>
+                    <div className="itens">
+                      <img src={Img_icon_checked.src} className="img-item-icon"/>
+                      <span className="specialist-position">Execução de camadas de reforço de camadas de reforço e subleito tratado.</span>
+                    </div>
+                    <div className="itens">
+                      <img src={Img_icon_checked.src} className="img-item-icon"/>
+                      <span className="specialist-position">Escavações em solo e rocha.</span>
                     </div>
                   </div>
-                  {/* item */}
-                  <div className="itens-card">
-                    <div className="content-title">
-                      <img src={Img_icon.src} className="img-item-icon" />
-                      <h2 className="title">Terraplanagem</h2>
+                </div>
+                <div className="itens-card">
+                  <div className="content-title">
+                    <img src={Img_icon.src} className="img-item-icon"/>
+                    <h2 className="title">Terraplanagem</h2>
+                  </div>
+                  <div className="content-itens">
+                    <div className="itens">
+                      <img src={Img_icon_checked.src} className="img-item-icon"/>
+                      <span className="specialist-position">Escavações em solo e rocha.</span>
                     </div>
-                    <div className="content-itens">
-                      <div className="itens">
-                        <img src={Img_icon_checked.src} className="icon-check" />
-                        <span className="specialist-position">Escavações em solo e rocha.</span>
-                      </div>
-                      <div className="itens">
-                        <img src={Img_icon_checked.src} className="icon-check" />
-                        <span className="specialist-position">Execução de camadas de reforço de camadas de reforço e subleito tratado.</span>
-                      </div>
-                      <div className="itens">
-                        <img src={Img_icon_checked.src} className="icon-check" />
-                        <span className="specialist-position">Escavações em solo e rocha.</span>
-                      </div>
+                    <div className="itens">
+                      <img src={Img_icon_checked.src} className="img-item-icon"/>
+                      <span className="specialist-position">Execução de camadas de reforço de camadas de reforço e subleito tratado.</span>
+                    </div>
+                    <div className="itens">
+                      <img src={Img_icon_checked.src} className="img-item-icon"/>
+                      <span className="specialist-position">Escavações em solo e rocha.</span>
                     </div>
                   </div>
-                  {/* item */}
-                  <div className="itens-card">
-                    <div className="content-title">
-                      <img src={Img_icon.src} className="img-item-icon" />
-                      <h2 className="title">Terraplanagem</h2>
+                </div>
+                
+                <div className="itens-card">
+                  <div className="content-title">
+                    <img src={Img_icon.src} className="img-item-icon"/>
+                    <h2 className="title">Terraplanagem</h2>
+                  </div>
+                  <div className="content-itens">
+                    <div className="itens">
+                      <img src={Img_icon_checked.src} className="img-item-icon"/>
+                      <span className="specialist-position">Escavações em solo e rocha.</span>
                     </div>
-                    <div className="content-itens">
-                      <div className="itens">
-                        <img src={Img_icon_checked.src} className="icon-check" />
-                        <span className="specialist-position">Escavações em solo e rocha.</span>
-                      </div>
-                      <div className="itens">
-                        <img src={Img_icon_checked.src} className="icon-check" />
-                        <span className="specialist-position">Execução de camadas de reforço de camadas de reforço e subleito tratado.</span>
-                      </div>
-                      <div className="itens">
-                        <img src={Img_icon_checked.src} className="icon-check" />
-                        <span className="specialist-position">Escavações em solo e rocha.</span>
-                      </div>
+                    <div className="itens">
+                      <img src={Img_icon_checked.src} className="img-item-icon"/>
+                      <span className="specialist-position">Execução de camadas de reforço de camadas de reforço e subleito tratado.</span>
+                    </div>
+                    <div className="itens">
+                      <img src={Img_icon_checked.src} className="img-item-icon"/>
+                      <span className="specialist-position">Escavações em solo e rocha.</span>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </section>
+        </div>
+      </section>
     </div>
   );
 }
